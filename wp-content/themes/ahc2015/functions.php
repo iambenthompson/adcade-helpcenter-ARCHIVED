4f4a7f75a0d87c75f201b91f0b75672497f74c8a
Ben Thompson <bt@adcade.com>
2015-10-05 18:18:11 -0400

    Adds enqueue script call for adscript api since I forgot to before

--- wp-content/themes/ahc2015/functions.php
+++ wp-content/themes/ahc2015/functions.php
@@ -471,6 +471,8 @@ function ahc2015_scripts() {
 
 	wp_enqueue_script( 'ahc2015-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
+    wp_enqueue_script( 'ahc2015-adscript-api', get_template_directory_uri() . '/js/adscript-api.js', array(), '20150908', true );
+
 	wp_enqueue_script( 'ahc2015-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
