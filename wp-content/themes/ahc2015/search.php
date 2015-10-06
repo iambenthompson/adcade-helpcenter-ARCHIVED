cfc16292732c455acdc1ed4016aeb3c239e23b22
Ben Thompson <bt@adcade.com>
2015-10-06 12:02:10 -0400

    Updates search page

--- wp-content/themes/ahc2015/search.php
+++ wp-content/themes/ahc2015/search.php
@@ -13,9 +13,12 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 		<?php if ( have_posts() ) : ?>
-
+			<?php 
+				global $wp_query;
+				$number_of_results = $wp_query->found_posts;
+			?>
 			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'ahc2015' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+				<h1 class="page-title"><?php printf( esc_html__( 'Found ' . $number_of_results . ' search result' . ($number_of_results > 1 ? 's' : '') . ' for: %s', 'ahc2015' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
 			</header><!-- .page-header -->
 
 			<?php /* Start the Loop */ ?>
@@ -32,8 +35,8 @@ get_header(); ?>
 
 			<?php endwhile; ?>
 
-			<?php the_posts_navigation(); ?>
-
+			<?php //the_posts_navigation(); ?>
+			<?php wp_pagenavi(); ?>
 		<?php else : ?>
 
 			<?php get_template_part( 'template-parts/content', 'none' ); ?>
@@ -43,5 +46,5 @@ get_header(); ?>
 		</main><!-- #main -->
 	</section><!-- #primary -->
 
-<?php get_sidebar(); ?>
+<?php //get_sidebar(); ?>
 <?php get_footer(); ?>
--- wp-content/themes/ahc2015/template-parts/content-search.php
+++ wp-content/themes/ahc2015/template-parts/content-search.php
@@ -11,9 +11,44 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+		<?php 
+		$post_type = get_post_type();
+		$meta = "";
+		$link = get_permalink();
+		switch ($post_type)
+		{
+			case "adscript-api":
+				//$meta = " [API: Class]";
+				break;
+			case "method":
+				$adscript_api_ID = get_post_meta(get_the_ID(), "_wpcf_belongs_adscript-api_id")[0];
+				$link = post_permalink($adscript_api_ID) . "#" . get_the_title() . "()";
+				//$meta = " [API: Method of the " . ahc2015_adscript_link_html($adscript_api_ID) . " Class]";
+				break;
+			case "property":
+				$adscript_api_ID = get_post_meta(get_the_ID(), "_wpcf_belongs_adscript-api_id")[0];
+				$link = post_permalink($adscript_api_ID) . "#" . get_the_title();
+				//$meta = " [API: Property of the " . ahc2015_adscript_link_html($adscript_api_ID) . " Class]";
+				break;
+			case "parameter":
+				$method_ID = get_post_meta(get_the_ID(), "_wpcf_belongs_method_id")[0];
+				$adscript_api_ID = get_post_meta($method_ID, "_wpcf_belongs_adscript-api_id")[0];
+				$link = post_permalink(get_post_meta($method_ID, "_wpcf_belongs_adscript-api_id")[0]) . "#" . get_the_title($method_ID) . "()";
+				//$meta = " [API: Parameter of the " . ahc2015_adscript_link_html($adscript_api_ID, get_the_title($method_ID), get_the_title($method_ID) . "()") . " Method in the " . ahc2015_adscript_link_html($adscript_api_ID) . " Class]";
+				break;
+			case "parameter-key":
+				$parameter_ID = get_post_meta(get_the_ID(), "_wpcf_belongs_parameter_id")[0];
+				$method_ID = get_post_meta($parameter_ID, "_wpcf_belongs_method_id")[0];
+				$adscript_api_ID = get_post_meta($method_ID, "_wpcf_belongs_adscript-api_id")[0];
+				$link = post_permalink(get_post_meta($method_ID, "_wpcf_belongs_adscript-api_id")[0]) . "#" . get_the_title($method_ID) . "()";
+				//$meta = " [API: Part of the " . ahc2015_adscript_link_html($adscript_api_ID, get_the_title($parameter_ID), get_the_title($method_ID) . "()") . " Parameter of the " . ahc2015_adscript_link_html($adscript_api_ID, get_the_title($method_ID), get_the_title($method_ID) . "()") . " Method in the " . ahc2015_adscript_link_html($adscript_api_ID) . " Class]";
+				break;
+		}
+		$meta = " <i> - " . str_replace(home_url(), '', $link) . "</i>"; 
+		?>
+		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( $link ) ), '</a>' . $meta . '</h2>' ); ?>
 
-		<?php if ( 'post' == get_post_type() ) : ?>
+		<?php if ( 'post' == $post_type ) : ?>
 		<div class="entry-meta">
 			<?php ahc2015_posted_on(); ?>
 		</div><!-- .entry-meta -->
