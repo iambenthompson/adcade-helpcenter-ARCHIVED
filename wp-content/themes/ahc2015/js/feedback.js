a92d51ade07e4b084fce1d08e6115d85ade2f192
Ben Thompson <bt@adcade.com>
2015-10-12 14:16:23 -0400

    Adds rate-limiting and friendly Thank You message to page feedback UI

--- wp-content/themes/ahc2015/js/feedback.js
+++ wp-content/themes/ahc2015/js/feedback.js
@@ -6,11 +6,15 @@
 
 jQuery(document).ready(function () {
 	// YES button
-	jQuery('.page-feedback-voting button.positive').click(function(){
+	jQuery('.page-feedback-voting').on('click', 'button.positive', function(){
 		__gaTracker('send', 'event', 'Page Feedback', 'Vote', 'Positive', 1);
+		jQuery('.page-feedback-voting button').off('click').remove();
+		jQuery('.page-feedback-voting p').append('<strong>Thank you!</strong>');
 	});
 	// NO button
-	jQuery('.page-feedback-voting button.negative').click(function(){
+	jQuery('.page-feedback-voting').on('click', 'button.negative', function(){
 		__gaTracker('send', 'event', 'Page Feedback', 'Vote', 'Negative', -1);
+		jQuery('.page-feedback-voting button').off('click').remove();
+		jQuery('.page-feedback-voting p').append('<strong>Thank you!</strong>');
 	});
 });
